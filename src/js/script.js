ef00b401d744d930903d942adb15e5563de462c0
Esnaux-Virgile <virgile.esnaux@edu.univ-fcomte.fr>
2025-12-02 13:39:59 +0100

    javascipt complété

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,13 @@
 // *** LIGHTBOX ***
+const lightbox = document.querySelector("#lightbox");
+
+const lightboxImage = document.querySelector("#lightbox img");
+
+lightbox?.addEventListener("click", () => lightbox.close());
+
+document.body.addEventListener("click", (evt) => {
+    if (!evt.target.matches("[data-full-img]")) return;
+    lightboxImage.src = evt.target.dataset.fullImg;
+    lightbox.showModal();
+});
+// *** END LIGHTBOX ***
